f0d49078fdb05ae82d41ad196545697a43cc3b8b
whitehat16081 <72308381+whitehat16081@users.noreply.github.com>
2021-04-12 23:15:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,7 +2,10 @@ var Runner,img
 var coin,img1
 var bad,img2
 var car,img3
-var bus,img4
+var bus,img4,img5;
+var Score=0;
+var gameState="play";
+
 
 function preload(){
 img=loadImage("Runner.png");
@@ -10,34 +13,30 @@ img1=loadImage("Coin.png");
 img2=loadImage("bad.png");
 img3=loadImage("car.png");
 img4=loadImage("Bus.png");
+img5=loadImage("bg.jpg");
 }
 
 function setup() {
-  createCanvas(1100,750);
+  createCanvas(1100,600);
 
+  obstacleGroup=new Group();
+  coinGroup=new Group();
   bad=createSprite(500,200,10,10);
   bad.addImage(img2);
   bad.scale=2
   bad.velocityX=-2
   
-  
-
-
-  Runner=createSprite(100,350,10,10);
+    Runner=createSprite(100,350,10,10);
   Runner.addImage(img)
   Runner.scale=0.3
-
-
-
-  
-
-
-
+  Runner.debug=true;
+  Runner.setCollider("rectangle",0,0,780,300);
 }
 
 function draw() {
- background("black");
  
+ if(gameState==="play"){
+background("black");
  spawnObstacles();
  spawnCoin();
 
@@ -49,37 +48,54 @@ if(bad.x<500){
    // Movement of the Runner 
 
 if(keyDown("RIGHT_ARROW")){
-  Runner.x=Runner.x+5
+  Runner.x=Runner.x+9
 }
 
 if(keyDown("LEFT_ARROW")){
-  Runner.x=Runner.x-6
+  Runner.x=Runner.x-9
 }
 
 if(keyDown("UP_ARROW")){
-  Runner.y=Runner.y-5
+  Runner.y=Runner.y-9
 }
 
 if(keyDown("DOWN_ARROW")){
-  Runner.y=Runner.y+6
+  Runner.y=Runner.y+9
 }
-//if(coin.isTouching(Runner)){
-  //coin.velocityX=0
-//}
 
+for(var i=0;i<coinGroup.length;i++){
+  if(coinGroup.get(i).isTouching(Runner)){
+    coinGroup.get(i).destroy();
+    Score=Score+1;
+ }
+}
 
-
-
- 
+for(var i=0;i<obstacleGroup.length;i++){
+  if(obstacleGroup.get(i).isTouching(Runner)){
+    //obstacleGroup.get(i).destroy();
+    gameState="end";
+    
+ }
+}
    
     drawSprites();
+    textSize(30);
+    fill("white");
+    text("Score:"+Score,800,50);
+ }
 
-
+else if(gameState==="end"){
+   background(img5);
+    
+ }
+   
 }
 
 function spawnObstacles(){
   if(frameCount%100===0){
     var obstacle=createSprite(1200,500,50,50);
+    obstacle.debug=true;
+    //obstacle.setCollider("rectangle",0,0,780,300);
     obstacle.velocityX=-5;
     obstacle.y=Math.round(random(50,700));
     var rand=Math.round(random(1,2));
@@ -90,6 +106,7 @@ function spawnObstacles(){
       obstacle.addImage(img4);
     }
     obstacle.scale=0.5;
+    obstacleGroup.add(obstacle);
   }
 }
 
@@ -98,7 +115,12 @@ function spawnCoin(){
     coin=createSprite(1000,150,10,10);
     coin.addImage(img1);
     coin.scale=0.3
+    coin.debug=true;
+    coin.setCollider("rectangle",0,0,280,300);
     coin.velocityX=-5;
     coin.y=Math.round(random(50,700));
+      coinGroup.add(coin);
   }
 }
+
+
